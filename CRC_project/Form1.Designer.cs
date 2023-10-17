2cb91834c0a4211e1b57684692fdf40af38b8fe1
KX <wenxuexiang@wenxuexiang-qrs.cis.com>
2023-10-17 15:47:21 +0800

    上传文件夹

---
+++ CRC_project/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace CRC_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(36, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "6"});
+            this.comboBox1.Location = new System.Drawing.Point(36, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(36, 100);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(118, 100);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(203, 100);
+            this.textBox3.MaxLength = 2;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 29);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Visible = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(294, 100);
+            this.textBox4.MaxLength = 2;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 29);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.Visible = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox5.Location = new System.Drawing.Point(383, 100);
+            this.textBox5.MaxLength = 2;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 29);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.Visible = false;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox6.Location = new System.Drawing.Point(474, 100);
+            this.textBox6.MaxLength = 2;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(57, 29);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox6.Visible = false;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox7.Location = new System.Drawing.Point(36, 227);
+            this.textBox7.MaxLength = 2;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(57, 29);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox8.Location = new System.Drawing.Point(118, 227);
+            this.textBox8.MaxLength = 2;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(57, 29);
+            this.textBox8.TabIndex = 9;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 305);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CRC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ CRC_project/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CRC_project
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("crcdll.dll")]
+        public static extern int add(int a,int b);
+        [DllImport("crcdll.dll")]
+        public static extern int test(int[] a, int b);
+        [DllImport("crcdll.dll")]
+        public static extern ushort crc(ushort[] data, int len);
+        [DllImport("BMSDLL.dll")]
+        public static extern bool Open(int index);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] PEC_DATA = new byte[2];
+             
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+
+                    byte[] data = new byte[2];
+                    data[0] = Convert.ToByte(textBox1.Text, 16);
+                    data[1] = Convert.ToByte(textBox2.Text, 16);
+                    ushort[] PEC = new ushort[1];
+                    PEC = ByteTOUshort(data);
+                    ushort pec_data = crc1(PEC, 1);
+                    PEC_DATA = UshortTOByte(pec_data);
+
+                }
+                else
+                {
+                    byte[] data = new byte[6];
+                    data[0] = Convert.ToByte(textBox1.Text, 16);
+                    data[1] = Convert.ToByte(textBox2.Text, 16);
+                    data[2] = Convert.ToByte(textBox3.Text, 16);
+                    data[3] = Convert.ToByte(textBox4.Text, 16);
+                    data[4] = Convert.ToByte(textBox5.Text, 16);
+                    data[5] = Convert.ToByte(textBox6.Text, 16);
+                    ushort[] PEC = new ushort[3];
+                    PEC = ByteTOUshort(data);
+                    ushort pec_data = crc1(PEC, 3);
+                    PEC_DATA = UshortTOByte(pec_data);
+                }
+
+                textBox7.Text = PEC_DATA[0].ToString("x2");
+                textBox8.Text = PEC_DATA[1].ToString("x2");
+
+            }
+            catch
+            {
+
+            }
+           
+          
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+                textBox5.Visible = false;
+                textBox6.Visible = false;
+            }
+            else
+            {
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                textBox5.Visible = true;
+                textBox6.Visible = true;
+            }
+        }
+        void Input_limit(object sender)
+        {
+            TextBox tb = (TextBox)sender;
+            try
+            {
+                Convert.ToByte(tb.Text.Substring(tb.Text.Length - 1, 1), 16);
+
+            }
+            catch (Exception)
+            {
+                if (tb.Text.Length > 0)
+                {
+                    tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+       
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            Input_limit(sender);
+        }
+
+        public ushort[] ByteTOUshort(byte[] data)
+        {
+            int len = data.Length;
+            ushort[] CRC_Data = new ushort[len / 2];
+            byte array;
+
+            for (int i = 0; i < len; i += 2)
+            {
+                array = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = array;
+
+            }
+            for (int i = 0; i < len / 2; i++)
+            {
+                CRC_Data[i] = BitConverter.ToUInt16(data, 2 * i);
+            }
+            //    Console.WriteLine(" CRC_Data" + CRC_Data[0]);
+            for (int i = 0; i < len; i += 2)
+            {
+                array = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = array;
+
+            }
+            return CRC_Data;
+        }
+
+
+        byte[] UshortTOByte(ushort udata)
+        {
+            byte[] UTB = new byte[2];
+            UTB[1] = (byte)(udata | 0x00);
+            UTB[0] = (byte)((udata >> 8) | 0x00);
+            return UTB;
+        }
+
+
+        //pec计算
+        public ushort crc1(ushort[] data, byte len)
+        {
+            ushort poly = 0x4599;
+            ushort CRC = 0x0010;
+            for (uint i = 0; i < len; i++)
+            {
+                for (UInt16 k = 0; k < 16; k++)
+                {
+                    if (((CRC & 0x4000) << 1) == (data[i] & 0x8000))
+                    {
+                        CRC = (ushort)(CRC << 1);
+                        data[i] = (ushort)(data[i] << 1);
+                        //    Console.WriteLine("CRC1 = " + CRC);
+
+                    }
+                    else
+                    {
+                        CRC = (ushort)(CRC << 1);
+                        CRC = (ushort)(CRC ^ poly);
+                        CRC = (ushort)(CRC & 0x7fff);
+                        data[i] = (ushort)(data[i] << 1);
+                        //   Console.WriteLine("CRC2 = " + CRC);                  
+                    }
+
+                }
+            }
+
+            CRC = (ushort)(CRC << 1);
+            return CRC;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            /*ushort[] aaa = { 0x1122, 0x3344};
+
+            ushort[] bbb = { 0x1122, 0x3344};
+          
+            ushort d = crc1(bbb, 2);
+            ushort t = crc(aaa, 2);
+            int n = add(1, 2);*/
+
+
+            int[] arr = {2,1,3};
+            int b = DPLIS(arr);
+           /* BinaryTree tree = new BinaryTree(arr);
+            tree.Frontshow(tree.rootNode);
+            Console.WriteLine();
+            tree.ordershow(tree.rootNode);
+            Console.WriteLine();
+            tree.PosShow(tree.rootNode);
+            Console.WriteLine();*/
+            
+        }
+       
+        public int DPLIS(int[] a)
+        {
+            int sum = 0;
+            int maxsum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if(a[i] < a[j])
+                    {
+                        sum++;
+                        int newlen = a.Length - j;
+                        int[] temp = new int[newlen];
+                        Array.Copy(a, j, temp, 0, newlen);
+                        DPLIS(temp);
+
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+    
+    public class test
+    {
+        private int total_ic, pert;
+        public void set_total_ic(int total_ic)
+        {
+            this.total_ic = total_ic;
+        }
+        public int get_total_ic()
+        {
+            return this.total_ic;
+        }
+        public int set_get_total_ic(int total_ic)
+        {
+            this.total_ic = total_ic;
+            return this.total_ic;
+        }
+       
+    }
+
+    public class Node
+    {
+
+        public Node leftNode;
+        public Node rightNode;
+        public int item;
+        public Node(int item)
+        {
+
+            this.item = item;
+            leftNode = null;
+            rightNode = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+
+        public  Node rootNode;
+
+        public BinaryTree(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Add(arr[i]);
+            }
+        }
+
+        private void Add(int item)
+        {
+
+            Node currNode = rootNode;
+
+            if(rootNode == null)
+            {
+                rootNode = new Node(item);
+                return;
+            }
+
+            while (true)
+            {
+
+                if(item < currNode.item)
+                {
+
+                    if(currNode.leftNode == null)
+                    {
+                        currNode.leftNode = new Node(item);
+                        return;
+                    }
+                    else
+                    {
+                        currNode = currNode.leftNode;
+                    }
+                }
+                else
+                {
+                    if(currNode.rightNode == null)
+                    {
+
+                        currNode.rightNode = new Node(item);
+                        return;
+                    }
+                    else
+                    {
+
+                        currNode = currNode.rightNode;
+                    }
+                }
+
+            }
+
+
+        }
+
+
+        public void Frontshow(Node rootNode)
+        {
+
+            if(rootNode != null)
+            {
+
+                Console.Write(rootNode.item);
+                Frontshow(rootNode.leftNode);
+                Frontshow(rootNode.rightNode);
+            }
+        }
+
+        public void ordershow(Node rootNode)
+        {
+
+            if(rootNode != null)
+            {
+
+                ordershow(rootNode.leftNode);
+                Console.Write(rootNode.item);
+                ordershow(rootNode.rightNode);
+            }
+        }
+
+        public void PosShow(Node rootNode)
+        {
+            if (rootNode != null)
+            {
+                PosShow(rootNode.leftNode);
+                PosShow(rootNode.rightNode);
+                Console.Write(rootNode.item);
+            }
+        }
+
+    }
+   
+}
